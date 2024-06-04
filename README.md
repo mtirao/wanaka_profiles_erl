f0b861e4fa76ea46bfbb0c12c4ec0323d78ee3e6
Marcos Tirao <marcos.tirao@icloud.com>
2024-06-04 17:01:05 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+smartlist_server
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/smartlist_server_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc smartlist_server public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(smartlist_server_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    smartlist_server_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
